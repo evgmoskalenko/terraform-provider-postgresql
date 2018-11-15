6991e4ee09b6fb1a5e10c520c3221f144bbcca4d
Cyril Gaudin <cyril.gaudin@gmail.com>
2018-11-15 16:04:53 +0100

    Create new resource: postgresql_grant (#6)
    * Create new resource: postgresql_grant
    
    This resource allow to grant privileges on all existing tables or sequences
    for a specified role in a specified schema.

--- postgresql/helpers.go
+++ postgresql/helpers.go
@@ -1,8 +1,13 @@
 package postgresql
 
 import (
+	"database/sql"
 	"fmt"
 	"strings"
+
+	"github.com/hashicorp/errwrap"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/lib/pq"
 )
 
 // pqQuoteLiteral returns a string literal safe for inclusion in a PostgreSQL
@@ -21,4 +26,99 @@ func validateConnLimit(v interface{}, key string) (warnings []string, errors []e
 		errors = append(errors, fmt.Errorf("%s can not be less than -1", key))
 	}
 	return
+}
+
+func sliceContainsStr(haystack []string, needle string) bool {
+	for _, s := range haystack {
+		if s == needle {
+			return true
+		}
+	}
+	return false
+}
+
+// allowedPrivileges is the list of privileges allowed per object types in Postgres.
+// see: https://www.postgresql.org/docs/current/sql-grant.html
+var allowedPrivileges = map[string][]string{
+	"table":    []string{"ALL", "SELECT", "INSERT", "UPDATE", "DELETE", "TRUNCATE", "REFERENCES", "TRIGGER"},
+	"sequence": []string{"ALL", "USAGE", "SELECT", "UPDATE"},
+}
+
+// validatePrivileges checks that privileges to apply are allowed for this object type.
+func validatePrivileges(objectType string, privileges []interface{}) error {
+	allowed, ok := allowedPrivileges[objectType]
+	if !ok {
+		return fmt.Errorf("unknown object type %s", objectType)
+	}
+
+	for _, priv := range privileges {
+		if !sliceContainsStr(allowed, priv.(string)) {
+			return fmt.Errorf("%s is not an allowed privilege for object type %s", priv, objectType)
+		}
+	}
+	return nil
+}
+
+func pgArrayToSet(arr pq.ByteaArray) *schema.Set {
+	s := make([]interface{}, len(arr))
+	for i, v := range arr {
+		s[i] = string(v)
+	}
+	return schema.NewSet(schema.HashString, s)
+}
+
+// startTransaction starts a new DB transaction on the specified database.
+// If the database is specified and different from the one configured in the provider,
+// it will create a new connection pool if needed.
+func startTransaction(client *Client, database string) (*sql.Tx, error) {
+	if database != "" && database != client.databaseName {
+		var err error
+		client, err = client.config.NewClient(database)
+		if err != nil {
+			return nil, err
+		}
+	}
+	db := client.DB()
+	txn, err := db.Begin()
+	if err != nil {
+		return nil, errwrap.Wrapf("could not start transaction: {{err}}", err)
+	}
+
+	return txn, nil
+}
+
+func dbExists(txn *sql.Tx, dbname string) (bool, error) {
+	err := txn.QueryRow("SELECT datname FROM pg_database WHERE datname=$1", dbname).Scan(&dbname)
+	switch {
+	case err == sql.ErrNoRows:
+		return false, nil
+	case err != nil:
+		return false, errwrap.Wrapf("could not check if database exists: {{err}}", err)
+	}
+
+	return true, nil
+}
+
+func roleExists(txn *sql.Tx, rolname string) (bool, error) {
+	err := txn.QueryRow("SELECT 1 FROM pg_roles WHERE rolname=$1", rolname).Scan(&rolname)
+	switch {
+	case err == sql.ErrNoRows:
+		return false, nil
+	case err != nil:
+		return false, errwrap.Wrapf("could not check if role exists: {{err}}", err)
+	}
+
+	return true, nil
+}
+
+func schemaExists(txn *sql.Tx, schemaname string) (bool, error) {
+	err := txn.QueryRow("SELECT 1 FROM pg_namespace WHERE nspname=$1", schemaname).Scan(&schemaname)
+	switch {
+	case err == sql.ErrNoRows:
+		return false, nil
+	case err != nil:
+		return false, errwrap.Wrapf("could not check if schema exists: {{err}}", err)
+	}
+
+	return true, nil
 }
---
+++ postgresql/testing.go
@@ -0,0 +1,148 @@
+package postgresql
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/hashicorp/errwrap"
+)
+
+const (
+	dbNamePrefix     = "tf_tests_db"
+	roleNamePrefix   = "tf_tests_role"
+	testRolePassword = "testpwd"
+
+	testTableDef = "CREATE TABLE test_table (val text)"
+)
+
+func getTestConfig(t *testing.T) Config {
+	getEnv := func(key, fallback string) string {
+		value := os.Getenv(key)
+		if len(value) == 0 {
+			return fallback
+		}
+		return value
+	}
+
+	dbPort, err := strconv.Atoi(getEnv("PGPORT", "5432"))
+	if err != nil {
+		t.Fatalf("could not cast PGPORT value as integer: %v", err)
+	}
+
+	return Config{
+		Host:     getEnv("PGHOST", "localhost"),
+		Port:     dbPort,
+		Username: getEnv("PGUSER", ""),
+		Password: getEnv("PGPASSWORD", ""),
+		SSLMode:  getEnv("PGSSLMODE", ""),
+	}
+}
+
+// dbExecute is a test helper to create a pool, execute one query then close the pool
+func dbExecute(t *testing.T, dsn, query string, args ...interface{}) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		t.Fatalf("could to create connection pool: %v", err)
+	}
+	defer db.Close()
+
+	// Create the test DB
+	if _, err = db.Exec(query, args...); err != nil {
+		t.Fatalf("could not execute query %s: %v", query, err)
+	}
+}
+
+func getTestDBNames(dbSuffix string) (dbName string, roleName string) {
+	dbName = fmt.Sprintf("%s_%s", dbNamePrefix, dbSuffix)
+	roleName = fmt.Sprintf("%s_%s", roleNamePrefix, dbSuffix)
+
+	return
+}
+
+// setupTestDatabase creates all needed resources before executing a terraform test
+// and provides the teardown function to delete all these resources.
+func setupTestDatabase(t *testing.T, createDB, createRole, createTable bool) (string, func()) {
+	config := getTestConfig(t)
+
+	suffix := strconv.Itoa(int(time.Now().UnixNano()))
+
+	dbName, roleName := getTestDBNames(suffix)
+
+	if createDB {
+		dbExecute(t, config.connStr("postgres"), fmt.Sprintf("CREATE DATABASE %s", dbName))
+	}
+	if createRole {
+		dbExecute(t, config.connStr("postgres"), fmt.Sprintf(
+			"CREATE ROLE %s LOGIN ENCRYPTED PASSWORD '%s'",
+			roleName, testRolePassword,
+		))
+	}
+
+	if createTable {
+		// Create a test table in this new database
+		dbExecute(t, config.connStr(dbName), testTableDef)
+	}
+
+	return suffix, func() {
+		dbExecute(t, config.connStr("postgres"), fmt.Sprintf("DROP DATABASE IF EXISTS %s", dbName))
+		dbExecute(t, config.connStr("postgres"), fmt.Sprintf("DROP ROLE IF EXISTS %s", roleName))
+	}
+}
+
+func testCheckTablePrivileges(
+	t *testing.T, dbSuffix string, allowedPrivileges []string, createTable bool,
+) error {
+	config := getTestConfig(t)
+
+	dbName, roleName := getTestDBNames(dbSuffix)
+
+	// Some test (e.g.: default privileges) need the test table to be created only now
+	if createTable {
+		db, err := sql.Open("postgres", config.connStr(dbName))
+		if err != nil {
+			t.Fatalf("could not open connection pool for db %s: %v", dbName, err)
+		}
+		defer db.Close()
+
+		if _, err := db.Exec(testTableDef); err != nil {
+			t.Fatalf("could not create test table in db %s: %v", dbName, err)
+		}
+		// In this case we need to drop table after each test.
+		defer func() {
+			db.Exec("DROP TABLE test_table")
+		}()
+	}
+
+	// Connect as the test role
+	config.Username = roleName
+	config.Password = testRolePassword
+
+	db, err := sql.Open("postgres", config.connStr(dbName))
+	if err != nil {
+		t.Fatalf("could not open connection pool for db %s: %v", dbName, err)
+	}
+	defer db.Close()
+
+	queries := map[string]string{
+		"SELECT": "SELECT count(*) FROM test_table",
+		"INSERT": "INSERT INTO test_table VALUES ('test')",
+		"UPDATE": "UPDATE test_table SET val = 'test'",
+		"DELETE": "DELETE FROM test_table",
+	}
+
+	for queryType, query := range queries {
+		_, err := db.Exec(query)
+
+		if err != nil && sliceContainsStr(allowedPrivileges, queryType) {
+			return errwrap.Wrapf(fmt.Sprintf("could not %s on test table: {{err}}", queryType), err)
+
+		} else if err == nil && !sliceContainsStr(allowedPrivileges, queryType) {
+			return errwrap.Wrapf(fmt.Sprintf("%s did not failed as expected: {{err}}", queryType), err)
+		}
+	}
+	return nil
+}
